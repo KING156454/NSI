6f0e01f9a6ff6fb780e445f96309db7ac7c1ce6b
KING156454 <alex.133.b@gmail.com>
2025-09-25 09:33:30 +0200

    Update Piles_Files.py

--- liste chaine/Piles_Files.py
+++ liste chaine/Piles_Files.py
@@ -1,6 +1,5 @@
 from liste_chaine import Liste_chaine
 
-
 class Pile:
     def __init__(self):
         self.donnees = Liste_chainee()       
@@ -8,5 +7,16 @@ class Pile:
     def push(self, element):
         self.donnees.ajoute_debut(element)
 
+    def vide(self):
+        if self.donnees.debut is None:
+            return True
+        return False
+    
     def pop(self):
         self.donnees.sup_cellule()
+    
+    def pop2(self):
+        if not self.vide():
+            self.donnees.sup_cellule()
+        else:
+            pritn("liste vide")
