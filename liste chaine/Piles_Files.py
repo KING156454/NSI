87e28d033d90216b58e6a4434a04f1560ace69ad
KING156454 <alex.133.b@gmail.com>
2025-09-25 09:24:43 +0200

    Update Piles_Files.py

--- liste chaine/Piles_Files.py
+++ liste chaine/Piles_Files.py
@@ -1,1 +1,12 @@
 from liste_chaine import Liste_chaine
+
+
+class Pile:
+    def __init__(self):
+        self.donnees = Liste_chainee()       
+        
+    def push(self, element):
+        self.donnees.ajoute_debut(element)
+
+    def pop(self):
+        self.donnees.sup_cellule()
